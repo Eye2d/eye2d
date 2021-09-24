31d0f35d8bf3782c6121d7a2b750a60406eb5d7c
Administrator <Administrator@DESKTOP-NDQH6HI>
2021-09-24 17:49:23 +0700

    .

--- eye2d.app_data/Application/Softwares/Dtos/SoftwareEditRequest.cs
+++ eye2d.app_data/Application/Softwares/Dtos/SoftwareEditRequest.cs
@@ -6,5 +6,13 @@ namespace eye2d.app_data.Application.Softwares.Dtos
 {
     public class SoftwareEditRequest
     {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+        public string SeoAlias { get; set; }
+        public string LanguageId { set; get; }
     }
 }
--- eye2d.app_data/Entities/Product.cs
+++ eye2d.app_data/Entities/Product.cs
@@ -7,7 +7,7 @@ namespace eye2d.app_data.Entities
     public class Product
     {
         public int Id { set; get; }
-       public decimal Price { set; get; }
+        public decimal Price { set; get; }
         public decimal OriginalPrice { set; get; }
         public int Stock { set; get; }
         public int ViewCount { set; get; }
